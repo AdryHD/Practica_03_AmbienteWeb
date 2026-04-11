119926f318d329b49de44d83b5fd0517d42ec25c
Hilanai <hilagoncruz@gmail.com>
2026-04-11 11:32:21 -0600

    Utilitario update
    Utilitario update version mejorada

--- Models/UtilitarioModel.php
+++ Models/UtilitarioModel.php
@@ -1,12 +1,31 @@
 <?php
+<?php
 
-function OpenDatabase()
+class UtilitarioModel
 {
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    return mysqli_connect("127.0.0.1", "root", "", "practicas13", 3307);
-}
+    private static $host = '127.0.0.1';
+    private static $user = 'root';
+    private static $pass = '';
+    private static $db = 'practicas13';
+    private static $port = 3307;
 
-function CloseDatabase($context)
-{
-    mysqli_close($context);
+    public static function OpenDatabase()
+    {
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+        try {
+            $conn = mysqli_connect(self::$host, self::$user, self::$pass, self::$db, self::$port);
+            return $conn;
+        } catch (Exception $e) {
+            // Log error instead of exposing it
+            error_log('Database connection failed: ' . $e->getMessage());
+            return null;
+        }
+    }
+
+    public static function CloseDatabase($context)
+    {
+        if ($context) {
+            mysqli_close($context);
+        }
+    }
 }
